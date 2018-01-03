2322019a786bb6d78d5df44317069f5dc090ed0b
Sudarshan jhawar <sudarshan.jhawar@oitcolitcaz.com>
2018-01-03 13:01:38 +0530

    files added

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import { Car } from "./classes/Car.js";
+import { Drone } from "./classes/Drone.js";
+
+let c = new Car();
+let d = new Drone();
---
+++ src/classes/Car.js
@@ -0,0 +1,4 @@
+import { Vehicle } from "./Vehicle.js";
+export class Car entends Vehicle {
+
+}
